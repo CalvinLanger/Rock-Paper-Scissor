dacfcc23adedaf8f178354abd81ea4db8d455cdc
Calvin Langer <calvin.langerr@gmail.com>
2021-12-10 15:37:27 +0100

    add footer current date and variables

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log('Welcome in my little game, enjoy it!');
+
+//Game Variables
+const userScore = 0;
+const computerScore = 0;
+const userScore_Span = document.getElementById('user-score');
+const computerScore_Span = document.getElementById('computer-score');
+const scoreBoard = document.querySelector('.score-board');
+const result = document.querySelector('.results')
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+
+//Footer current Date
+const today = new Date();
+const year = today.getUTCFullYear();
+document.getElementById('current-date').innerHTML = year;
+
