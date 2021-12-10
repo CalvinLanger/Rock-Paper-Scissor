efb060b9c78c5a306b7a97c4d74737a2fadfb5fc
Calvin Langer <calvin.langerr@gmail.com>
2021-12-10 16:22:29 +0100

    add computer and user choices

--- app.js
+++ app.js
@@ -11,16 +11,28 @@ const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    console.log("userChoice => " + userChoice);
+    console.log("computerChoice => " + computerChoice);
+}
+
 rock.addEventListener('click', function() {
-    console.log('rock');
+    game('rock');
 });
 
 paper.addEventListener('click', function() {
-    console.log('paper');
+    game('paper');
 });
 
 scissors.addEventListener('click', function() {
-    console.log('scissors');
+    game('scissors');
 });
 
 //Footer current Date
